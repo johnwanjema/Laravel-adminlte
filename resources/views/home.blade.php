7a1a370ccf1d5b9153049e533e477cda0477a235
John Wanjema <jonwanjema@gmail.com>
2022-06-19 18:55:43 -0700

    update href links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="index3.html" class="nav-link">Home</a>
+                    <a href="javascript:void(0)" class="nav-link">Home</a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
                     <a href="#" class="nav-link">Contact</a>
@@ -154,7 +154,7 @@
 
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
-            <a href="index3.html" class="brand-link">
+            <a href="javascript:void(0)" class="brand-link">
                 <img src=" https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">AdminLTE 3</span>
             </a>
@@ -194,19 +194,19 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="./index.html" class="nav-link active">
+                                    <a href="javascript:void(0)" class="nav-link active">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Dashboard v1</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="./index2.html" class="nav-link">
+                                    <a href="javascript:void(0)" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Dashboard v2</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="./index3.html" class="nav-link">
+                                    <a href="javascript:void(0)" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Dashboard v3</p>
                                     </a>
@@ -214,7 +214,7 @@
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ route('logout') }}" class="nav-link" style="color:red">
+                            <a href="{{ route('logout') }}" class="nav-link" style="color:#DC3945">
                                 <i class="nav-icon fas fa-lock"></i>
                                 <p>
                                     Logout
